f56a613dd117713d28d4855a6d5db55705286124
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-09-09 17:08:14 +0300

    test api renamed

--- src/main/java/com/h3project/server/H3Controller.java
+++ src/main/java/com/h3project/server/H3Controller.java
@@ -10,7 +10,7 @@ public class H3Controller {
     @Autowired
     H3Service h3Service;
 
-    @RequestMapping(value = "/getGeojsonByQueryTest", method = RequestMethod.GET)
+    @RequestMapping(value = "/quakestr-dataset-as-geojson", method = RequestMethod.GET)
     public String getGeojsonByQueryTest() {
         return this.h3Service.getGeojsonByQuery("select * from quakes_tr");
     }
