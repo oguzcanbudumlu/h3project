94cc28ad47aec3d028d0b7b1789c0b0e9008aaaa
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-09-10 20:04:53 +0300

    uncompleted async method example added

---
+++ src/main/java/com/example/asyncmethod/GithubLookupService.java
@@ -0,0 +1,26 @@
+package com.example.asyncmethod;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class GithubLookupService {
+
+    private final RestTemplate restTemplate;
+
+    public GithubLookupService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    @Async
+    public CompletableFuture<User> findUser(String user) throws InterruptedException {
+        String url = String.format("https://api.github.com/users/%s", user);
+        User results = restTemplate.getForObject(url, User.class);
+        Thread.sleep(1000L);
+        return CompletableFuture.completedFuture(results);
+    }
+}
