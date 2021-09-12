c431ecf27f8bb32a1965ea7b684245143900da3d
Oğuzcan Budumlu <obudumlu@gmail.com>
2021-09-12 17:52:16 +0300

    h3 command line app added

---
+++ src/main/java/com/h3project/server/H3CommandLineApp.java
@@ -0,0 +1,31 @@
+package com.h3project.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+public class H3CommandLineApp implements CommandLineRunner {
+    private static Logger LOG = LoggerFactory.getLogger(H3CommandLineApp.class);
+
+    @Autowired
+    private H3Service h3Service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("H3CommandLineApp run method is starting");
+
+        long start = System.currentTimeMillis();
+        CompletableFuture<Object> geojson = h3Service.asyncTest("select * from quakes_tr");
+        CompletableFuture.allOf(geojson);
+        long end = System.currentTimeMillis();
+
+        LOG.info("Elapsed time: " + (end-start)/1000.0 + " s");
+        LOG.info("--> " + geojson.get());
+
+    }
+}
